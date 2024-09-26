b77fb1bbba25898b3a9b4904515656c7a3e8ee45
DaliKallali <kallali.daly@gmail.com>
2024-09-26 19:07:22 +0100

    updated backend myUser myRestaurant

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
 import morgan from "morgan";
-import UserRoute from "./routes/UserRoute";
+import MyUserRoute from "./routes/MyUserRoute";
+import MyRestaurantRoute from "./routes/MyRestaurantRoute";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,12 +14,18 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to database"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
 
-app.use("/api/my/user", UserRoute);
-
+app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", MyRestaurantRoute);
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
 });
